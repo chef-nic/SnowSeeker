32801feb97d1d47f6a1f218473506dfbc6a23bcc
chef-nic <nicholasajohnson@me.com>
2024-08-07 09:25:14 -0700

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Nicholas Johnson on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
